eb567801d0543f611be392aaebfd99480c36665d
zac1st1k <zacfirst@gmail.com>
2015-01-31 22:13:23 +1100

    + Setup DetailViewController

---
+++ TaskIt/TaskDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TaskDetailViewController.swift
+//  TaskIt
+//
+//  Created by Zac on 31/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
