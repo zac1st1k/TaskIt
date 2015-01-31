6de625e4027197cabfb414d0ffa6d8c31cfc177e
zac1st1k <zacfirst@gmail.com>
2015-01-31 22:00:47 +1100

    + TaskModel class

---
+++ TaskIt/TaskModel.swift
@@ -0,0 +1,15 @@
+//
+//  TaskModel.swift
+//  TaskIt
+//
+//  Created by Zac on 31/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+import Foundation
+
+struct TaskModel {
+    var task:String
+    var subTask:String
+    var date:String
+}
--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -11,12 +11,16 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    var taskArray:[TaskModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-//        tableView.dataSource = self
-//        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.delegate = self
+        let task1 = TaskModel(task: "Study French", subTask: "Verbs", date: "01/14/2014")
+        let task2 = TaskModel(task: "Eat Dinner", subTask: "Burgers", date: "01/14/2014")
+        taskArray = [task1, task2, TaskModel(task: "Gym", subTask: "Leg Day", date: "01/14/2014")]
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,13 +33,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return 1
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return taskArray.count
+
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath) as TaskCell
-        cell.taskLabel.text = "Study French"
-        cell.descriptionLabel.text = "Verbs in past and present"
-        cell.dateLabel.text = "01/10/2014"
+        cell.taskLabel.text = taskArray[indexPath.row].task
+        cell.descriptionLabel.text = taskArray[indexPath.row].subTask
+        cell.dateLabel.text = taskArray[indexPath.row].date
         return cell
     }
     func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
