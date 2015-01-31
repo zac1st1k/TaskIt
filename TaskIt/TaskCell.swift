8e574541e70672f19bc3c0cd6235c201a4172899
zac1st1k <zacfirst@gmail.com>
2015-01-31 21:46:58 +1100

    + Setup storyboad + Custom Task class + Table view delegate

---
+++ TaskIt/TaskCell.swift
@@ -0,0 +1,17 @@
+//
+//  TaskCell.swift
+//  TaskIt
+//
+//  Created by Zac on 31/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+    
+    @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+}
--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        tableView.dataSource = self
+//        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // UITableView Delegate Methods
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath) as TaskCell
+        cell.taskLabel.text = "Study French"
+        cell.descriptionLabel.text = "Verbs in past and present"
+        cell.dateLabel.text = "01/10/2014"
+        return cell
+    }
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 
 }
 
